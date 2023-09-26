f6dad99f7317c966639377b2a1d43e71c4e66527
Julius John <julius.john@elev.ga.ntig.se>
2023-09-26 10:45:58 +0200

    new

--- Combat Game v1/Program.cs
+++ Combat Game v1/Program.cs
@@ -1,26 +1,31 @@
-﻿int Health1 = 100;
+﻿using System.Runtime.CompilerServices;
+
+int Health1 = 100;
 int Health2 = 100;
 
 //randomizer
 
 Random generator = new Random();
 
-int randonumber = 6 + generator.Next(10);
-
-Console.WriteLine(randonumber);
-
+int randonumber = 6 + generator.Next(11);
 //characters
-Console.WriteLine();
+Console.WriteLine("Please State your name player:");
+string name = Console.ReadLine().ToLower();
+Console.WriteLine($"Welcome to HyperBattles {name} press enter to commence the fight!");
+Console.ReadLine();
+Console.WriteLine("You have entered the battle");
+Console.ReadLine();
 
+//coinflip
+Random coinflips = new Random();
+int coinflip = coinflips.Next(1,3);
 
-
+Console.WriteLine("To determine who goes first we will do a coinflip, you may chose H or T");
+string flip =Console.ReadLine().ToLower();
+int H = 1;
+int T = 2;
+int right;
+right == coinflip;
 //while loop
-
-while (randonumber > 0)
-{
-    Console.WriteLine(randonumber);
-     += randonumber;
-
-}
 
 Console.ReadLine();
