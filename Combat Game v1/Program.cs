480e6b46a1844444c4503efa173a4c23d3ce7ce7
Julius John <julius.john@elev.ga.ntig.se>
2023-09-28 12:15:48 +0200

    Uppdate Method Features added

--- Combat Game v1/Program.cs
+++ Combat Game v1/Program.cs
@@ -1,11 +1,5 @@
-﻿int Health1 = 100;
-int Health2 = 100;
+﻿
 
-//randomizer
-
-Random generator = new Random();
-
-int randonumber = 6 + generator.Next(11);
 //characters
 Console.WriteLine("Please State your name player:");
 string name = Console.ReadLine().ToLower();
@@ -18,12 +12,54 @@ Console.ReadLine();
 Random coinflips = new Random();
 int coinflip = coinflips.Next(1,3);
 
-Console.WriteLine("To determine who goes first we will do a coinflip, you may chose H or T");
-string flip =Console.ReadLine().ToLower();
-int H = 1;
-int T = 2;
-int right;
-right == coinflip;
-//while loop
+Console.WriteLine("To determine who goes first we will do a coinflip, you may chose 1 or 2 (1=heads and 2=tails)");
+int choice = int.Parse(Console.ReadLine());
+
+Random generator = new Random();
+
+int randonumber = 6 + generator.Next(11);
+
+if (choice == coinflip)
+{
+  YouGoFirst(name);
+  Console.ReadLine();
+  ApplyDamageToEnemy(randonumber);
+
+}
+else 
+{
+    EnemyGoFirst(name);
+    Console.ReadLine();
+    ApplyDamageToSelf(randonumber);
+}
+
 
 Console.ReadLine();
+
+
+//methods
+
+
+static void ApplyDamageToSelf(int damage)
+{
+    int urhp = 100;
+    urhp -= damage;
+    Console.WriteLine($"Your enemy has done {damage} damage to you!");
+}
+
+static void ApplyDamageToEnemy(int damage)
+{
+    int enemyhp = 100;
+    enemyhp -= damage;
+    Console.WriteLine($"You have done {damage} damage to your enemy!");
+}
+
+static void YouGoFirst(string name)
+{
+    Console.WriteLine($"Congrats {name} you get to go first! press enter to continue");
+}
+
+static void EnemyGoFirst(string name)
+{
+    Console.WriteLine($"Unfortunately {name} you were incorrect, your enemy goes first! press enter to continue");
+}
